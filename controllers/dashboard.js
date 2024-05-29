02b444cb70d4a09b8991a8ab848a9803f93d328a
Aysenur Kiymaz <147588960+Aysenurkiymaz@users.noreply.github.com>
2024-05-29 13:27:38 +0200

    Update dashboard.js

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,13 +1,41 @@
+const Event = require('../model/event');
+const Guest = require('../model/guest');
+
 module.exports = {
     viewEvent: (req, res) => {
         res.render('event');
     },
 
     dashboardView: (req, res) => {
-        res.render('dashboard');
+        const events = Event.findAll({
+            include: [Guest] // Guest modelini de dahil et
+        })
+        res.render('dashboard', {
+            events: events
+        });
     },
-    createEvent: (req, res) => {
-        res.render('event');
-    },
+    createEvent: async (req, res) => {
 
+        const event = await Event.create({
+            name: req.body.name,
+            organization: req.body.organization,
+            date: req.body.date,
+        });
+        event.save();
+
+        for (const guestName of req.body.guestList) {
+            const guest = await Guest.create({
+                name: guestName,
+                event: event,
+            });
+            guest.save();
+        }
+        event.save();
+
+        console.log('Request Body: ', event);
+        res.json(event);
+        res.end();
+       // sequelize.events
+    },
+    
 }
