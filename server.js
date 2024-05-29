1771ea0fd0e98907a4914ce4f2ff44102e5ad878
Aysenur Kiymaz <147588960+Aysenurkiymaz@users.noreply.github.com>
2024-05-29 13:34:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const { db } = require('./db');
+const Event = require('./model/event');
+const Guest = require('./model/guest');
+
+Event.hasMany(Guest);
+Guest.belongsTo(Event, { as: 'event' });
+
 const dashboardRoutes = require('./routes/dashboard');
-const db = require('./db.js');
 
 const app = express();
 const PORT = 8080;
 
-app.listen(PORT, () => console.log('Server is running on port: ' + PORT));
-
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'pug');
 app.use('/', dashboardRoutes);
 
-db.sync({force: false})
+db.sync()
     .then(() => {
-        // app.listen(PORT, console.log('Server is running on port: ' + PORT));
-    });
+        app.listen(PORT, console.log('Server is running on port: ' + PORT));
+    }); 
